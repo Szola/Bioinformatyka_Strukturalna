7c8221d7528534471e63324eda638eeb2df56ae5
Szola <szola.szymczak@gmail.com>
2016-06-13 15:42:11 +0200

    -

--- program.py
+++ program.py
@@ -4,18 +4,28 @@ import subprocess
 print("Podaj nazwe czasteczki: ")
 nazwa_czast = input()
 nazwa_pliku = nazwa_czast + '.pdb'
-czy_zbadany = 0
+plikPDB = 1
 
 sc = '/home/szola/Dokumenty/Studia/Bioinformatyka_RNA/Bioinformatyka_Strukturalna/'
 
-if os.path.isfile(sc + '/' + nazwa_czast + '/MC-Ann-' + nazwa_czast):
-    czy_zbadany = 0
+if os.path.isdir(sc + nazwa_czast):
+    plikPDB = 1
+else:
+    os.mkdir(sc + nazwa_czast)
+
+if os.path.isfile(sc + nazwa_czast + '/' + nazwa_pliku):
+    plikPDB = 1
+else:
+    print("Nie istanieje plik " + nazwa_pliku + " konieczny do zadziałania programu.")
+    print("Do folderu " + nazwa_czast + " dodaj plik " + nazwa_pliku + ". Mozna go pobrac ze strony http://www.rcsb.org/pdb/home/home.do")
+    plikPDB = 0
 
 
 def u_prog(): # uruchamia programy RNAVIEW i MCAnnotate
     with open(sc + nazwa_czast + "/MC-Ann-" + nazwa_czast, "w") as f:
-        subprocess.call([sc + 'MC-Annotate', sc + nazwa_czast + '/' + nazwa_pliku])
+        subprocess.call([sc + 'MC-Annotate', sc + nazwa_czast + '/' + nazwa_pliku], stdout=f)
     subprocess.run([sc + '/RNAVIEW/bin/rnaview', sc + nazwa_czast + '/' + nazwa_pliku])
+
 
     #legenda wynikow:
     #plik "MC-Ann-_" plik wyjsciowy programu MCAnnotate
@@ -26,7 +36,6 @@ def u_prog(): # uruchamia programy RNAVIEW i MCAnnotate
 #def u_bd(): #
 #    return 0
 
-
-if czy_zbadany == 0:
+if ( plikPDB != 0):
     u_prog()
 #u_bd()
