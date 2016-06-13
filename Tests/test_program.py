4e57446162d1a9aceda4fbd8f99da83a441262b8
Szola <szola.szymczak@gmail.com>
2016-06-13 22:29:21 +0200

    poprawione testy

--- Tests/test_program.py
+++ Tests/test_program.py
@@ -1,6 +1,6 @@
 import unittest
 from os import getcwd, remove, listdir
-from os.path import sep, isfile
+from os.path import sep, isfile, basename
 import subprocess
 
 
@@ -10,30 +10,32 @@ class ProgramTests(unittest.TestCase):
 
 
     def test_mc_annotate(self):
-        nazwa_czast = "2n7x"
-        sc_pliku_wej = getcwd() + sep + nazwa_czast + ".pdb"
-        sc_pliku_wyj = getcwd() + sep + "MC-Ann-" + nazwa_czast
-        with open(sc_pliku_wyj, "w") as f:
-            subprocess.call([self.sc + 'MC-Annotate', sc_pliku_wej], stdout=f)
+        nazwa_czasteczek = [basename(f).split(".")[0] for f in listdir('.') if isfile(f) and ".pdb" in f]
+        for nazwa_czast in nazwa_czasteczek:
+            sc_pliku_wej = getcwd() + sep + nazwa_czast + ".pdb"
+            sc_pliku_wyj = getcwd() + sep + "MC-Ann-" + nazwa_czast
+            with open(sc_pliku_wyj, "w") as f:
+                subprocess.call([self.sc + 'MC-Annotate', sc_pliku_wej], stdout=f)
 
-        self.assertTrue(isfile(sc_pliku_wyj))
+            self.assertTrue(isfile(sc_pliku_wyj))
 
-        with open(sc_pliku_wyj, "r") as f:
-            lines = f.read()
-            self.assertTrue(lines)
+            with open(sc_pliku_wyj, "r") as f:
+                lines = f.read()
+                self.assertTrue(lines)
 
-        remove(sc_pliku_wyj)
+            remove(sc_pliku_wyj)
 
 
     def test_rnaview(self):
-        nazwa_czast = "2n7x"
-        sc_pliku = getcwd() + sep + nazwa_czast + ".pdb"
-        subprocess.run([self.sc + sep + 'RNAVIEW/bin/rnaview', sc_pliku])
+        nazwa_czasteczek = [basename(f).split(".")[0] for f in listdir('.') if isfile(f) and ".pdb" in f]
+        for nazwa_czast in nazwa_czasteczek:
+            sc_pliku = getcwd() + sep + nazwa_czast + ".pdb"
+            subprocess.run([self.sc + sep + 'RNAVIEW/bin/rnaview', sc_pliku])
 
-        self.assertTrue(isfile(sc_pliku + ".out") or isfile(sc_pliku + "_nmr.pdb.out"))
+            self.assertTrue(isfile(sc_pliku + ".out") or isfile(sc_pliku + "_nmr.pdb.out"))
 
-        for file in [f for f in listdir('.') if isfile(f) and ((".out" in f) or ("pdb_nmr.pdb" in f))]:
-            remove(file)
+            for file in [f for f in listdir('.') if isfile(f) and ((".out" in f) or ("pdb_nmr.pdb" in f))]:
+                remove(file)
 
 
 
